8623e2e2f8ee6fffbfa5ecf9ea93bb743fc5898e
troushoo <troushoo@gmail.com>
2016-02-21 13:56:29 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SimulatedMemoryLeak
+{
+    class Program
+    {
+        static Foo[] array;
+        static int i;
+
+        static void Main(string[] args)
+        {
+            array = new Foo[2048];
+            i = 0;
+
+            Console.WriteLine("Press Enter to allocate large memory.");
+            Console.WriteLine("Press Ctrl + C to exit");
+
+            while (true)
+            {
+                Console.ReadLine();
+                array[i] = new Foo();
+                i++;
+                Console.WriteLine("10M bytes are allocated.");
+            }
+        }
+
+        public class Foo
+        {
+            private byte[] m_buffer = new byte[10240000]; //Allocate 10M bytes
+        }
+    }
+}
