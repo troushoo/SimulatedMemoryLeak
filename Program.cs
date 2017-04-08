c86c7a44bae4588c32ccb850cd0338e5d39e548e
tr <troushoo@gmail.com>
2017-04-09 00:12:54 +0900

    brush-up

--- Program.cs
+++ Program.cs
@@ -1,32 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SimulatedMemoryLeak
 {
     class Program
     {
-        static Foo[] array;
-        static int i;
-
         static void Main(string[] args)
         {
-            array = new Foo[2048];
-            i = 0;
+            List<byte[]> mybytelist = new List<byte[]>();
+            int i = 0;
 
             Console.WriteLine("Press Enter to allocate large memory.");
             Console.WriteLine("Press Ctrl + C to exit");
 
             while (true)
             {
-                Console.ReadLine();
-                array[i] = new Foo();
-                i++;
-                Console.WriteLine("10M bytes are allocated.");
-            }
-        }
+                Console.Read();
+                mybytelist.Add(new byte[10240000]); //Allocate 10M bytes
 
-        public class Foo
-        {
-            private byte[] m_buffer = new byte[10240000]; //Allocate 10M bytes
+                for (int j = 0; j < mybytelist[i].Length; j++)
+                {
+                    (mybytelist[i])[j] = 255; //assign data
+                }
+                i++;
+            }
+            
+            
+
         }
     }
 }
